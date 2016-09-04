e82fe4c6190658354be7455dbfcce986b81c5bd3
oesgalha <oscaresgalha@gmail.com>
2016-09-04 04:48:10 -0300

    Styleshed register page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,3 +272,12 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
+
+# append to end of config/initializers/devise.rb
+Rails.application.config.to_prepare do
+  Devise::SessionsController.layout "devise"
+  Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? "application" : "devise" }
+  Devise::ConfirmationsController.layout "devise"
+  Devise::UnlocksController.layout "devise"
+  Devise::PasswordsController.layout "devise"
+end
