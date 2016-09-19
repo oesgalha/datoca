4fc76be9ae13e1f5457be63aae4bac08138e026f
oesgalha <oscaresgalha@gmail.com>
2016-09-18 22:37:56 -0300

    Clean tests folder

--- test/models/instruction_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class InstructionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/pride'
+require 'laranja'
+Laranja.load('pt-BR')
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def random_record(record_class)
+    record_class.offset(rand(record_class.count)).first
+  end
 end
