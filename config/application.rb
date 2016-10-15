e2d49f1a4b14284109dbe75188c68b11b7129d04
oesgalha <oscaresgalha@gmail.com>
2016-10-15 03:22:54 -0300

    Basic mail settings

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,14 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Datoca
+  def self.config
+    @config ||= OpenStruct.new(
+      YAML.load_file(
+        File.join(Rails.root, 'config', 'app.yml')
+      )[Rails.env]
+    )
+  end
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -14,13 +22,15 @@ module Datoca
 
     config.i18n.available_locales = [:en, :'pt-BR']
     config.i18n.default_locale = :'pt-BR'
-  end
 
-  def self.defaults
-    @defaults ||= OpenStruct.new(
-      YAML.load_file(
-        File.join(Rails.root, 'config', 'app.yml')
-      )[Rails.env]
-    )
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:        Datoca.config.dig('smtp', 'address'),
+      port:           Datoca.config.dig('smtp', 'port'),
+      domain:         Datoca.config.dig('smtp', 'domain'),
+      user_name:      Datoca.config.dig('smtp', 'user'),
+      password:       Datoca.config.dig('smtp', 'pass'),
+      authentication: Datoca.config.dig('smtp', 'auth')
+    }
   end
 end
