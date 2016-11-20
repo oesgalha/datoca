34d01f2328f4b458b874c074056cb77aebbbdc43
oesgalha <oscaresgalha@gmail.com>
2016-11-20 16:23:36 -0200

    :lipstick: provide images fallback w/ emojione

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,20 @@ module ApplicationHelper
       HTML
     end.join.html_safe
   end
+
+  def image_tag_with_fallback(resource, size)
+    class_size = "image is-#{size}x#{size}"
+    case resource.class.to_s
+    when 'Competition'
+      attachment_name = :ilustration
+      fallback_name = 'fallback-competition.svg'
+    when 'User'
+      attachment_name = :avatar
+      fallback_name = 'fallback-user.svg'
+    when 'Team'
+      attachment_name = :avatar
+      fallback_name = 'fallback-team.svg'
+    end
+    attachment_image_tag(resource, attachment_name, :fill, size, size, class: class_size, fallback: fallback_name)
+  end
 end
