91d452452bc50c6211af95181262de1d3993b290
oesgalha <oscaresgalha@gmail.com>
2017-01-30 10:53:51 -0200

    Happy path integration test for competition submissions

--- test/integration/make_submission_test.rb
+++ test/integration/make_submission_test.rb
@@ -7,8 +7,27 @@ class MakeSubmissionTest < ActionDispatch::IntegrationTest
     Capybara.current_driver = :poltergeist
   end
 
-  test "An user can submit" do
+  test 'An user can submit' do
     alice = create(:user)
-    # TODO
+    competition = create_competition_with_data
+
+    # Sign in
+    sign_in alice
+    visit root_path
+
+    # Look for the competition and enter it
+    click_on(competition.name)
+    click_on('Submeter')
+
+    # Fill the form
+    assert_difference [->{ Submission.count }, ->{ competition.submissions.count }] do
+      attach_file('Arquivo csv', random_csv)
+      click_on('Criar Submissão')
+    end
+
+    # Ensure the user is redirected to the submission summary page
+    page.assert_selector('h1.title', text: 'Resumo de submissões', visible: true)
+    page.assert_selector('h2.subtitle', text: competition.name, visible: true)
+    assert_equal summary_competition_submissions_path(competition), page.current_path
   end
 end
